15c0cbd62f9143568dd1315f5c8613c37c66d4d9
Craig Fowler <craig@wearegiants.com>
2015-08-25 12:46:50 -0700

    register update

---
+++ header-form.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en"> <!--<![endif]-->
+<head>
+
+<!--
+      ___          ___          ___          ___
+     /  /\        /  /\        /  /\        /  /\         ___
+    /  /::\      /  /::\      /  /::\      /  /:/_       /  /\
+   /  /:/\:\    /  /:/\:\    /  /:/\:\    /  /:/ /\     /  /:/
+  /  /:/~/:/   /  /:/  \:\  /  /:/  \:\  /  /:/ /::\   /  /:/
+ /__/:/ /:/___/__/:/ \__\:\/__/:/ \__\:\/__/:/ /:/\:\ /  /::\
+ \  \:\/:::::/\  \:\ /  /:/\  \:\ /  /:/\  \:\/:/~/://__/:/\:\
+  \  \::/~~~~  \  \:\  /:/  \  \:\  /:/  \  \::/ /:/ \__\/  \:\
+   \  \:\       \  \:\/:/    \  \:\/:/    \__\/ /:/       \  \:\
+    \  \:\       \  \::/      \  \::/       /__/:/         \__\/
+     \__\/        \__\/        \__\/        \__\/
+
+-->
+
+<meta charset="utf-8">
+<title><?php wp_title(' | ', true, 'right'); ?></title>
+<meta name="description" content="<?php bloginfo( 'description' ) ?>">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="shortcut icon" href="/assets/img/favicon.ico">
+<link href='http://fonts.googleapis.com/css?family=PT+Serif:400,400italic' rel='stylesheet' type='text/css'>
+
+<!--[if lt IE 9]>
+<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<script src="//use.typekit.net/wtq6lgl.js"></script>
+<script>try{Typekit.load();}catch(e){}</script>
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class('fs-grid splash'); ?>>
+  <div class="wrapper">
+    
--- page-form.php
+++ page-form.php
@@ -1,13 +1,35 @@
-<?php Themewrangler::setup_page();get_header('splash'/***Template Name: Register */); ?>
+<?php Themewrangler::setup_page();get_header('form'/***Template Name: Register */); ?>
+
+
+<?php 
+
+   $thumb_id = get_post_thumbnail_id();
+   $thumb_url_array = wp_get_attachment_image_src($thumb_id, 'original', true);
+   $thumb_url = $thumb_url_array[0];
+
+?>
+
+<style>
+   
+   body {
+      background: #111 url(<?php echo $thumb_url; ?>) center center!important;
+      background-size: cover!important;
+   }
+
+</style>
 
 <div id="register">
    <div class="fs-row">
       <div class="fs-cell fs-lg-8 fs-md-5 fs-sm-3 fs-centered">
          <hr class="invisible">
-         <h1 class="title"><?php the_title(); ?></h1>
-         <?php the_post(); the_content(); ?>
+         <div id="register--form_wrap">
+            <h1 class="title"><?php the_title(); ?></h1>
+            <?php the_post(); the_content(); ?>
+         </div>
       </div>
    </div>
 </div>
+
+<div id="register-bg"></div>
 
 <?php get_footer(); ?>
